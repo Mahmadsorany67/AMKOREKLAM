c7a054ba687cb802b3bcfd34182348b5824af880
Mahmadsorany67 <82833453+Mahmadsorany67@users.noreply.github.com>
2022-04-09 00:02:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -36,10 +36,10 @@ if(message.channel.type === 'dm') {
 
 if (message.content.startsWith("https://discord.gg/")) {
     
-  message.author.send(` دانرا تۆش ڕیکلامەکە بکە https://discord.gg/AkYA68Z2gG`) // لینکی سێرڤەری خۆت لیرە دابنی بە هەتا هەتای بێت
+  message.author.send(` دانرا تۆش ڕیکلامەکە بکە https://discord.gg/K3RP76AS7Q`) // لینکی سێرڤەری خۆت لیرە دابنی بە هەتا هەتای بێت
     
-  client.channels.get("841792552314994709").send(
-`> <a:E14567BBEE164342AF58DF6872011807:853287588390633502> By: <@${message.author.id}> ✅ 
+  client.channels.get("947911356508667904").send(
+`>  By: <@${message.author.id}> ✅ 
 ${message.content}`)
 }
     }
